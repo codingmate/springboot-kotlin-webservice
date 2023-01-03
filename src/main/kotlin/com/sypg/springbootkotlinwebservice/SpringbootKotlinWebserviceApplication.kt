8029339b59365b08403db7cf7052b0cfcaad252d
codingmate <hsykys0728@gmail.com>
2023-01-03 21:15:20 +0900

    Create Project

---
+++ src/main/kotlin/com/sypg/springbootkotlinwebservice/SpringbootKotlinWebserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.sypg.springbootkotlinwebservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinWebserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootKotlinWebserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/sypg/springbootkotlinwebservice/SpringbootKotlinWebserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sypg.springbootkotlinwebservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinWebserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
