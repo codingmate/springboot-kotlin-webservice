374dcf9fb9914f567b31fc6eb2e06215adb8f0bf
codingmate <hsykys0728@gmail.com>
2023-01-10 22:22:15 +0900

    change table name : post -> posts

--- src/main/kotlin/com/sypg/app/domain/posts/Posts.kt
+++ src/main/kotlin/com/sypg/app/domain/posts/Posts.kt
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
 
-@Table(name = "post")
+@Table(name = "posts")
 @Entity
 data class Posts ( // must have no arg contructor
     @Id
