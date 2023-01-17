93ad797eaddc342e3701c005b52b83b786760aff
계정 <hsykys0728@gmail.com>
2023-01-17 23:10:21 +0900

    using @AfterAll ( + @TestInstance )

--- src/test/kotlin/com/sypg/app/domain/posts/PostsRepositoryTest.kt
+++ src/test/kotlin/com/sypg/app/domain/posts/PostsRepositoryTest.kt
@@ -1,13 +1,25 @@
 package com.sypg.app.domain.posts
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS) // test 클래스 당 하나의 인스턴스 <- BeforeAll, AfterAll 사용을 위함
+//@TestInstance(Lifecycle.PER_METHOD) // test 메서드 당 하나의 인스턴스 생성
+
 //@Transactional // Rollback을 위함. 테스트 데이터가 반복적으로 입력될 때 대비?
 class PostsRepositoryTest ( @Autowired val postsRepository: PostsRepository) {
+
+    @AfterAll // 메서드가 static이거나 @TestInstance(Lifecycle.PER_CLASS)를 명시해야한다.
+              // 코틀린은 static method가 없다고 한다.
+    fun cleanup() {
+        postsRepository.deleteAll()
+    }
 
     @Test
     fun `게시글저장 불러오기`() {
